c9f5c07d7eb2978fd23e5347bdb894652f0c1789
giulio dalmasson <giulio.dalmasson@digitalglue.com.au>
2020-10-09 16:48:14 +1100

    create order , manage order

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import Cart from './components/Cart';
-import Filter from './components/Filter';
-import Products from './components/Products';
 import store from './store';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
+
+import HomeScreen from "./screens/HomeScreen";
+import AdminScreen from "./screens/AdminScreen";
 
 class App extends React.Component {
- 
   render() {
     return (
       <Provider store={store}>
-        <div className='grid-container'>
-          <header>
-            <a href='/'>React Ecommerce</a>
-          </header>
-          <main>
-            <div className='content'>
-              <div className='main'>
-                <Filter />
-                <Products/>
-              </div>
-              <div className='sidebar'>
-                <Cart/>
-              </div>
-            </div>
-          </main>
-          <footer>All rights reserved</footer>
-        </div>
+        <BrowserRouter>
+          <div className='grid-container'>
+            <header>
+              <Link to='/'>React Ecommerce</Link>
+              <Link to='/admin'>Admin</Link>
+            </header>
+            <main>
+              <Route path="/admin" component={AdminScreen} />
+              <Route path="/" exact={true} component={HomeScreen} />
+            </main>
+            <footer>All rights reserved</footer>
+          </div>
+        </BrowserRouter>
       </Provider>
     );
   }
